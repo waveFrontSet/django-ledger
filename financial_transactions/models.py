9a140bd3dd52a3bace3f1f4191c32d31277c3835
Paul Grillenberger <grillenbergerpaul@gmail.com>
2024-07-27 14:06:21 +0200

    Add party model

--- financial_transactions/models.py
+++ financial_transactions/models.py
@@ -1,1 +1,10 @@
-# Create your models here.
+from django.db import models
+
+
+class Party(models.Model):
+    """Party of a financial transaction."""
+
+    name = models.CharField(primary_key=True, max_length=100)
+
+    def __str__(self) -> str:
+        return self.name
